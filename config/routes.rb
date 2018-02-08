017b4fedbc655ebe6fbf6a7c05282de87cfef4a7
Geoff Humphreys <humphreys.geoff@gmail.com>
2018-02-08 17:31:58 -0500

    Configured custom routes for blog edit

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
-  resources :blogs
+  resources :blogs, exept: [:show, :edit]
+  get 'blog/:id', to: 'blogs#show', as: 'blog_show'
+  get 'blog/:id/edit', to: 'blogs#edit', as: 'blog_edit'
+
+
+
 
   root to: 'pages#home'
 end
