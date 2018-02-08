f1699b1e10807d89d9f6841c9f4ab943a17139b4
Geoff Humphreys <humphreys.geoff@gmail.com>
2018-02-08 18:28:09 -0500

    Updated status for enum

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,6 +2,9 @@ class Blog < ApplicationRecord
   has_many :blog_contents
   accepts_nested_attributes_for :blog_contents,
                                 reject_if: lambda { |attrs| attrs['main_image'].blank? && attrs['thumb_image'].blank? && attrs['body'].blank? }
+
+  enum status: { draft: 0, published: 1 }
+
   extend FriendlyId
-  friendly_id :title, use: :slugged                              
+  friendly_id :title, use: :slugged
 end
