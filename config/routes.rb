bbf830aecca7fd4ec6414e4bb68e5be983623a2c
Geoff Humphreys <humphreys.geoff@gmail.com>
2018-02-08 18:47:28 -0500

    Implemented custom action for updating status of blog post

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
   get 'blog/:id', to: 'blogs#show', as: 'blog_show'
   get 'blog/:id/edit', to: 'blogs#edit', as: 'blog_edit'
 
-
+  resources :blogs do
+  member do
+    get :toggle_status
+  end
+end
 
 
   root to: 'pages#home'
