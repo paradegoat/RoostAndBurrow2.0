70dceb82e3510354e170c31bcd6b6a4cd3abd501
Geoff Humphreys <humphreys.geoff@gmail.com>
2018-02-08 18:19:11 -0500

    Integrated friendlyID routes for blog post

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,6 @@ class Blog < ApplicationRecord
   has_many :blog_contents
   accepts_nested_attributes_for :blog_contents,
                                 reject_if: lambda { |attrs| attrs['main_image'].blank? && attrs['thumb_image'].blank? && attrs['body'].blank? }
+  extend FriendlyId
+  friendly_id :title, use: :slugged                              
 end
