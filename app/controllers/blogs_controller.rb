b7a5c28a65ddbde007c779b5706ee5e93be81c25
Geoff Humphreys <humphreys.geoff@gmail.com>
2018-02-19 13:59:52 -0500

    Added topic_id and status to strong params for blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -81,6 +81,6 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:title, :subtitle, blog_contents_attributes: [:main_image, :thumb_image, :body])
+      params.require(:blog).permit(:title, :subtitle, :topic_id, :status, blog_contents_attributes: [:main_image, :thumb_image, :body])
     end
 end
