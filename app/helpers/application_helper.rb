711aca0d5851558c22cfcbe17e5a5753e8750668
Geoff Humphreys <humphreys.geoff@gmail.com>
2018-02-20 13:25:12 -0500

    Implemented nav bar for blog layout and fixed bug with login helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,54 @@
 module ApplicationHelper
-  def login_helper
+  def login_helper style = ''
    if current_user.is_a?(GuestUser)
-     (link_to "Register", new_user_registration_path) +
-     "<br>".html_safe +
-     (link_to "Login", new_user_session_path)
+     (link_to "Register", new_user_registration_path, class: style) +
+     " ".html_safe +
+     (link_to "Login", new_user_session_path, class: style)
    else
-     link_to "Logout", destroy_user_session_path, method: :delete
+     link_to "Logout", destroy_user_session_path, method: :delete, class: style
    end
  end
 
- def source_helper(layout_name)
+ def source_helper(styles)
    if session[:source]
-     greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
-     content_tag(:p, greeting, class: "source-greeting")
+     greeting = "Thanks for visiting me from #{session[:source]}, please feel free to #{ link_to 'contact me', contact_path } if you'd like to work together."
+     content_tag(:div, greeting.html_safe, class: styles)
    end
  end
+
+ def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_me_path,
+        title: 'About Me'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: blogs_path,
+        title: 'Blog'
+      }
+    ]
+  end
+
+  def nav_helper style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
+
 end
