47371b226e00f21dcea22953bc29da69d34ad74b
Geoff Humphreys <humphreys.geoff@gmail.com>
2018-02-21 14:19:59 -0500

    Added blog_status_color to blog helper and added call in admin_actions

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,4 +2,12 @@ module BlogsHelper
   def gravatar_helper user
     image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 60
   end
+
+  def blog_status_color blog
+    if blog.draft?
+      'color: red;'
+    else
+    'color: green;'
+  end
+  end
 end
