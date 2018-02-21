7a7b99a2c5ca26a1953c6d770f600fe75c8914f2
Geoff Humphreys <humphreys.geoff@gmail.com>
2018-02-21 12:56:16 -0500

    Implemented custom scope for recent blogs and hide draft posts from non admins

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,14 +1,20 @@
 class Blog < ApplicationRecord
-  validates_presence_of :title, :subtitle
-  has_many :comments, dependent: :destroy
-  has_many :blog_contents, dependent: :destroy
-  belongs_to :topic
-  accepts_nested_attributes_for :blog_contents,
-                                allow_destroy: true,
-                                reject_if: lambda { |attrs| attrs['main_image'].blank? && attrs['thumb_image'].blank? && attrs['body'].blank? }
-
   enum status: { draft: 0, published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
 
+  validates_presence_of :title, :subtitle
+  has_many :comments, dependent: :destroy
+  has_many :blog_contents, dependent: :destroy
+
+  belongs_to :topic
+
+  accepts_nested_attributes_for :blog_contents,
+                                allow_destroy: true,
+                                reject_if: lambda { |attrs| attrs['main_image'].blank? && attrs['thumb_image'].blank? && attrs['body'].blank? }
+
+
+  def self.recent
+    order("created_at DESC")
+  end
 end
