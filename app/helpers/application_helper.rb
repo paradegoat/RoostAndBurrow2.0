28463844cc262b4e417a1dcc8d67862a3db9893d
Geoff Humphreys <humphreys.geoff@gmail.com>
2018-02-22 11:53:55 -0500

    Added new blog path to login_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,9 @@ module ApplicationHelper
      " ".html_safe +
      (link_to "Login", new_user_session_path, class: style)
    else
-     link_to "Logout", destroy_user_session_path, method: :delete, class: style
+     (link_to "Logout", destroy_user_session_path, method: :delete, class: style ) +
+     " ".html_safe +
+     (link_to 'Write a New Blog', new_blog_path, class: style)
    end
  end
 
