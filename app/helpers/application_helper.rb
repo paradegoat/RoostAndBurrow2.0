b318d8af7d6cff33877d98abf259d75847843440
Geoff Humphreys <humphreys.geoff@gmail.com>
2018-02-26 12:55:11 -0500

    Adjusted style of nav bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,6 @@
 module ApplicationHelper
   def login_helper style = ''
    if current_user.is_a?(GuestUser)
-     (link_to "Register", new_user_registration_path, class: style) +
-     " ".html_safe +
      (link_to "Login", new_user_session_path, class: style)
    else
      (link_to "Logout", destroy_user_session_path, method: :delete, class: style ) +
@@ -29,12 +27,12 @@ module ApplicationHelper
         title: 'About Me'
       },
       {
-        url: contact_path,
-        title: 'Contact'
-      },
-      {
         url: blogs_path,
         title: 'Blog'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
       }
     ]
   end
