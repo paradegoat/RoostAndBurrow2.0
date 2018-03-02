3e8fc5c278a2386af7c74fdb3b939ff0b665c8f9
Geoff Humphreys <humphreys.geoff@gmail.com>
2018-03-02 16:23:35 -0500

    Integrated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://roost-and-burrow.herokuapp.com', 'http://roost-and-burrow.herokuapp.com']
+  config.action_cable.url = "wss://roost-and-burrow.herokuapp.com/cable"
 end
