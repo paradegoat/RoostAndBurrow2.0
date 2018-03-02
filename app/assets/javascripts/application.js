8a0158239bc8f4cc2747cc1ede8c2aa25bfe0d3d
Geoff Humphreys <humphreys.geoff@gmail.com>
2018-03-02 11:52:36 -0500

    Implemented topic icons for sidebar and adjusted instafeed layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,11 +25,11 @@ $(document).ready(function() {
     var userFeed = new Instafeed({
         get: 'user',
         userId: '13341574',
-        limit: 6,
+        limit: 9,
         resolution: 'thumbnail',
         accessToken: '13341574.1677ed0.6ab6d995e85e47aea3903cf813219cef',
         sortBy: 'most-recent',
-        template: '<div class=" card gallery"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
+        template: '<div class="instagram-card col-lg-4"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
     });
     userFeed.run();
 
