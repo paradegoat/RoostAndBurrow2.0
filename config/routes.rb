f69fcf78977a7dcf043a0ed3c16336b5317d4d67
Geoff Humphreys <humphreys.geoff@gmail.com>
2018-03-07 17:05:22 -0500

    fixed spelling error in routes and styled topic icon

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
-  resources :blogs, exept: [:show, :edit]
+  resources :blogs, except: [:show, :edit]
   get 'blog/:id', to: 'blogs#show', as: 'blog_show'
   get 'blog/:id/edit', to: 'blogs#edit', as: 'blog_edit'
 
